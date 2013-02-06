0aa829caf6d02a291e454c456074db47c6deb646
Raymond Fallon <git@niclic.com>
2013-02-05 19:32:30 -0500

    Add Listing19 - Understanding Dependencies.

---
+++ source/vs2010/Chapter02/Listing19.cs
@@ -0,0 +1,48 @@
+﻿namespace Chapter02
+{
+    public class Listing19
+    {
+        public class Gear
+        {
+            public int Chainring { get; private set; }
+            public int Cog { get; private set; }
+            public int Rim { get; private set; }
+            public double Tire { get; private set; }
+
+            public Gear(int chainring, int cog, int rim, double tire)
+            {
+                Chainring   = chainring;
+                Cog         = cog;
+                Rim         = rim;
+                Tire        = tire;
+            }
+
+            public double GearInches()
+            {
+                return (Ratio() * new Wheel(Rim, Tire).Diameter());
+            }
+
+            public double Ratio()
+            {
+                return (Chainring / (double)Cog);
+            }
+        }
+
+        public class Wheel
+        {
+            public int Rim { get; private set; }
+            public double Tire { get; private set; }
+
+            public Wheel(int rim, double tire)
+            {
+                Rim = rim;
+                Tire = tire;
+            }
+
+            public double Diameter()
+            {
+                return Rim + (Tire * 2);
+            }
+        }
+    }
+}
---
+++ source/vs2010/Chapter02Tests/Listing19Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+using Gear = Chapter02.Listing19.Gear;
+
+namespace Chapter02Tests
+{
+    public class Listing19Tests : TestBase
+    {
+        [Test]
+        public void GearInches_WithGearDependantOnWheel_CalculatesGearInches()
+        {
+            var gear = new Gear(
+                NumberOfChainringTeeth, 
+                NumberOfCogTeeth, 
+                RimDiameterInInches, 
+                TireDiameterInInches
+                );
+
+            Assert.AreEqual(ExpectedGearInches, gear.GearInches());
+        }
+    }
+
+    public abstract class TestBase
+    {
+        protected readonly int NumberOfChainringTeeth = 52;   // number of teeth in chainring
+        protected readonly int NumberOfCogTeeth = 11;         // number of teeth in cog
+
+        protected readonly int RimDiameterInInches = 26;         // diameter of wheel rim in inches
+        protected readonly double TireDiameterInInches = 1.5d;   // diameter of tire in inches
+
+        protected readonly double ExpectedGearRatio = 4.7272727272727275d;   // gear ratio = chainring / cog
+        protected readonly double ExpectedGearInches = 137.09090909090909d;  // gear inches = (rim diameter + (tire diameter * 2)) * gear ratio
+        protected readonly double ExpectedWheelCircumference = 91.106186954104d;     // wheel diameter * PI
+    }
+}
