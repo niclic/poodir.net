9b440b4d2a649ada161ee70b9c84afd4d1c90471
Raymond Fallon <git@niclic.com>
2013-02-09 20:43:24 -0500

    Listing04 - Isolate Instance Creation.

---
+++ source/vs2010/Chapter02/Listing04.cs
@@ -0,0 +1,51 @@
+﻿namespace Chapter02
+{
+    // If you are so constrained that you cannot change the code to inject a Wheel 
+    // into a Gear, you should isolate the creation of Wheel inside the Gear class.
+    // This refactoring explicitly exposes the dependency, which can be removed later.
+    public class Listing04
+    {
+        public class Gear
+        {
+            public int Chainring { get; private set; }
+            public int Cog { get; private set; }
+            public Wheel Wheel { get; private set; }
+
+            // Note that a new instance of Wheel is created each time a new gear is created.
+            public Gear(int chainring, int cog, int rim, double tire)
+            {
+                Chainring = chainring;
+                Cog = cog;
+                Wheel = new Wheel(rim, tire);
+            }
+
+            // The responsibility for creating a Wheel instance is removed from the Gear Inches method.
+            public double GearInches()
+            {
+                return (Ratio() * Wheel.Diameter());
+            }
+
+            public double Ratio()
+            {
+                return (Chainring / (double)Cog);
+            }
+        }
+        // Unchanged from Listing03
+        public class Wheel
+        {
+            public int Rim { get; private set; }
+            public double Tire { get; private set; }
+
+            public Wheel(int rim, double tire)
+            {
+                Rim = rim;
+                Tire = tire;
+            }
+
+            public double Diameter()
+            {
+                return Rim + (Tire * 2);
+            }
+        }
+    }
+}
---
+++ source/vs2010/Chapter02Tests/Listing04Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+using Gear = Chapter02.Listing04.Gear;
+
+namespace Chapter02Tests
+{
+    [TestFixture]
+    public class Listing04Tests : TestBase
+    {
+        [Test]
+        public void GearInches_WhenWheelIsCreatedInGearConstructor_CalculatesGearInches()
+        {
+            var gear = new Gear(
+                NumberOfChainringTeeth, 
+                NumberOfCogTeeth, 
+                RimDiameterInInches, 
+                TireDiameterInInches);
+
+            Assert.AreEqual(ExpectedGearInches, gear.GearInches());
+        }
+    }
+}
